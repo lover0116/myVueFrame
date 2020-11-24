f8edb5717545bf7ce68924e5e2325cc38ce03134
xuzhi31672 <780272033@qq.com>
2020-11-24 16:12:57 +0800

    xxxxxxxxxxxxxx

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,15 +1,27 @@
 import axios from 'axios'
-import  Vue from 'vue'
-const $msg = Vue.prototype.$message;
+import {Message} from 'element-ui'
 
-axios.interceptors.response.use(function (res) {
-  if(res.data.success===false){
-    $msg.info(res.data.message||"null");
-  }
-  return res
-}, function (err) {
-  $msg.info(err.response.data.message);
-  return Promise.reject(err)
+axios.interceptors.request.use(config=> {
+  return config;
+}, err=> {
+  Message.error({message: '请求超时!'});
+  return Promise.resolve(err);
 })
-
+axios.interceptors.response.use(data=> {
+  if (data.status && data.status === 200 && data.data.status === 'error') {
+    Message.error({message: data.data.msg});
+    return;
+  }
+  return data;
+}, err=> {
+  console.log("err.response", err.response);
+  if (err.response.status === 504||err.response.status === 404) {
+    Message.error({message: '服务器被吃了⊙﹏⊙∥'});
+  } else if (err.response.status === 403) {
+    Message.error({message: '权限不足,请联系管理员!'});
+  }else {
+    Message.error({message: '未知错误!'});
+  }
+  return Promise.resolve(err);
+})
 export default axios
